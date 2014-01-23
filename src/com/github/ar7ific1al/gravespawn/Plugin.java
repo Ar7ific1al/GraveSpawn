b5e699807e8b24c078e29d36ec272a9026d401b1
Ar7ific1al <ar7ific1al@gmail.com>
2014-01-22 20:27:56 -0600

    Third test

--- src/com/github/ar7ific1al/gravespawn/Plugin.java
+++ src/com/github/ar7ific1al/gravespawn/Plugin.java
@@ -20,6 +20,7 @@ import com.github.ar7ific1al.gravespawn.utils.RespawnListener;
 public class Plugin extends JavaPlugin {
 	//	THIS IS A TEST FOR COMMIT FROM ECLIPSE! >:(
 	// HI AR7IFIC1AL!! :D
+	//	HI JAG :D
 	public static Logger console = Logger.getLogger("Minecraft");
 	public String version;
 
